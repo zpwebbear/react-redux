2ece92f343e41d1d810be720c0bff67b6a393df6
zpwebbear <zp.babu@gmail.com>
2021-03-14 14:50:55 +0200

    feat(ApiService): add FetchApi with tests

---
+++ src/api/fetch-api-service/FetchApi.test.js
@@ -0,0 +1,27 @@
+import { FetchApi } from "./FetchApi";
+
+describe("FetchApi Service", () => {
+  beforeAll(() => jest.spyOn(window, "fetch"));
+
+  test("Request method is called with the proper parameters", async () => {
+    const api = new FetchApi();
+
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({ success: true }),
+    });
+
+    const requestSpy = jest.spyOn(api, "_request");
+
+    await api.get("/test", {
+      method: "GET",
+      options: { Headers: { Authorization: "Basic validToken" } },
+    });
+
+    expect(requestSpy).toHaveBeenCalledWith({
+      urlPart: "/test",
+      method: "GET",
+      options: { Headers: { Authorization: "Basic validToken" } },
+    });
+  });
+});
