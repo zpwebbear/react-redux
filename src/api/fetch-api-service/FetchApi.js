ed0f388f59e0c303307d156317e77e98ed743559
zpwebbear <zp.babu@gmail.com>
2021-03-16 18:23:41 +0200

    fix(API): add headers to the FetchAPI

--- src/api/fetch-api-service/FetchApi.js
+++ src/api/fetch-api-service/FetchApi.js
@@ -1,10 +1,13 @@
 export function FetchApi() {
   this._baseApiUrl = process.env.REACT_APP_API_URL || "http://localhost";
-  this._baseApiHeaders = { Accept: "application/json" };
+  this._baseApiHeaders = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
 
   this._fetchToJson = async (url, ...args) => {
     const response = await window.fetch(url, ...args);
-    
+
     return response.json();
   };
 
@@ -21,30 +24,26 @@ export function FetchApi() {
     [...Object.keys(queryParams)].forEach((key) => {
       url.searchParams.set(key, queryParams[key]);
     });
-
     return url;
   };
 
   this._fetchOptions = function ({ method = "GET", body = {}, options = {} }) {
     return {
       method,
-      ...this._baseApiHeaders,
-      body: JSON.stringify(body),
+      headers: this._baseApiHeaders,
+      body: ["GET", "HEAD"].includes(method) ? undefined : JSON.stringify(body),
       ...options,
     };
   };
 
   this._request = async ({ urlPart, queryParams, method, body, options }) => {
-    const url = this._constructUrlWithQuery(urlPart, queryParams);
-    const mergedOptions = this._fetchOptions({ method: "GET", body, options })
+    const url = this._constructUrlWithQuery({ urlPart, queryParams });
+    const mergedOptions = this._fetchOptions({ method, body, options });
 
-    return this._fetchToJson(
-      url,
-      mergedOptions
-    );
+    return this._fetchToJson(url, mergedOptions);
   };
 
-  this.get = async (url, { ...args }) => {
+  this.get = async (url, args) => {
     return this._request({
       urlPart: url,
       method: "GET",
@@ -84,3 +83,5 @@ export function FetchApi() {
     });
   };
 }
+
+export const fetchAPI = new FetchApi();
